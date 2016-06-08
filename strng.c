c51fc8675d1b3ea56bb15e3bf5c73d7c954ececd
vinitha523 <vinithacomp@gmail.com>
2016-06-08 09:48:40 +0530

    Create strng.c

---
+++ strng.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    char *rev;
+    printf("Enter any string : ");
+    scanf("%s",str);
+    rev = strrev(str);
+   
+    printf("Reverse string is : %s",rev);
+   
+    return 0;
+}
